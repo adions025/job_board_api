4fccfae7aaa77524743cc28a498e88865d24874c
Adonis Gonzalez <adions025@gmail.com>
2021-09-06 17:11:19 +0200

    first commit

---
+++ backend/apis/general_pages/route_homepage.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="templates")
+general_pages_router = APIRouter()
+
+
+@general_pages_router.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse("general_pages/homepage.html", {"request": request})
---
+++ backend/core/config.py
@@ -0,0 +1,13 @@
+"""
+Settings class
+
+@author Adonis Gonzalez
+"""
+
+
+class Settings:
+    PROJECT_NAME: str = "Job Board"
+    PROJECT_VERSION: str = "1.0.0"
+
+
+settings = Settings()
