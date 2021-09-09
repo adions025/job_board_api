836bc229982b4f7209911dee0e37b79fbd8f4576
Adonis Gonzalez <adions025@gmail.com>
2021-09-09 15:58:48 +0200

    auth improvements!

--- backend/apis/utils.py
+++ backend/apis/utils.py
@@ -21,17 +21,47 @@ class OAuth2PasswordBearerWithCookie(OAuth2):
         flows = OAuthFlowsModel(password={"tokenUrl": tokenUrl, "scopes": scopes})
         super().__init__(flows=flows, scheme_name=scheme_name, auto_error=auto_error)
 
-    async def __call__(self, request: Request) -> Optional[str]:
-        authorization: str = request.cookies.get("access_token")  # changed to accept access token from httpOnly Cookie
-        print("access_token is", authorization)
+    # async def __call__(self, request: Request) -> Optional[str]:
+    #     authorization: str = request.cookies.get("access_token")  # changed to accept access token from httpOnly Cookie
+    #     print("access_token is", authorization)
+    #
+    #     scheme, param = get_authorization_scheme_param(authorization)
+    #     if not authorization or scheme.lower() != "bearer":
+    #         if self.auto_error:
+    #             raise HTTPException(
+    #                 status_code=status.HTTP_401_UNAUTHORIZED,
+    #                 detail="Not authenticated",
+    #                 headers={"WWW-Authenticate": "Bearer"},
+    #             )
+    #         else:
+    #             return None
+    #     return param
 
-        scheme, param = get_authorization_scheme_param(authorization)
+    async def __call__(self, request: Request) -> Optional[str]:
+        header_authorization: str = request.headers.get("Authorization")
+        cookie_authorization: str = request.cookies.get("Authorization")
+
+        header_scheme, header_param = get_authorization_scheme_param(header_authorization)
+        cookie_scheme, cookie_param = get_authorization_scheme_param(cookie_authorization)
+
+        if header_scheme.lower() == "bearer":
+            authorization = True
+            scheme = header_scheme
+            param = header_param
+
+        elif cookie_scheme.lower() == "bearer":
+            authorization = True
+            scheme = cookie_scheme
+            param = cookie_param
+
+        else:
+            authorization = False
+
         if not authorization or scheme.lower() != "bearer":
             if self.auto_error:
                 raise HTTPException(
-                    status_code=status.HTTP_401_UNAUTHORIZED,
-                    detail="Not authenticated",
-                    headers={"WWW-Authenticate": "Bearer"},
+                    status_code=status.HTTP_403_FORBIDDEN,
+                    detail="Not authenticated"
                 )
             else:
                 return None
